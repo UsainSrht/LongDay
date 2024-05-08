8257b3edfb3c065fb7289e37300342e6409ecdb3
UsainSrht <usainsrht@gmail.com>
2024-05-08 18:33:10 +0300

    plugin functionality

--- src/main/java/me/usainsrht/longday/LongDay.java
+++ src/main/java/me/usainsrht/longday/LongDay.java
@@ -1,17 +1,48 @@
 package me.usainsrht.longday;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
 
 public final class LongDay extends JavaPlugin {
 
+    HashMap<World, Integer> taskIDs;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        taskIDs = new HashMap<>();
 
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            //start timers when server is fully loaded.
+            for (World world : Bukkit.getWorlds()) {
+                if (world.getEnvironment() == World.Environment.NETHER || world.getEnvironment() == World.Environment.THE_END) continue;
+                if (!Boolean.parseBoolean(world.getGameRuleValue("doDaylightCycle"))) continue;
+                int taskID = Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+                    int i = 0;
+                    @Override
+                    public void run() {
+                        long time = world.getTime();
+                        // 12000>time>0 = daytime  --- every 2nd tick
+                        if (time > 0 && time < 12000 && i % 2 == 0) {
+                            world.setTime(time-1);
+                        } else if (i % 2 == 0) { //night --- every 2nd tick
+                            world.setTime(time+1);
+                        }
+                        i++;
+                        if (i > 2) i = 0;
+                    }
+                }, 1L, 1L).getTaskId();
+                taskIDs.put(world, taskID);
+            }
+        }, 1L);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        taskIDs.values().forEach(Bukkit.getScheduler()::cancelTask);
     }
+
+
 }
